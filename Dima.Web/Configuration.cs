df0a4ef2bad92d917e8691d4c5cd08821c9d29fd
marcusalves13 <marcusv.alves076@gmail.com>
2025-03-03 20:03:58 -0300

    Finalizando tela de login e configurando estilos

---
+++ Dima.Web/Configuration.cs
@@ -0,0 +1,36 @@
+﻿using MudBlazor;
+
+namespace Dima.Web;
+
+public static class Configuration
+{
+    public static MudTheme Theme = new()
+    {
+        Typography = new Typography
+        {
+            Default = new DefaultTypography()
+            {
+                FontFamily = ["Raleway", "sans-serif"]
+            }
+        },
+        PaletteDark = new PaletteDark()
+        {
+            Primary = Colors.LightGreen.Accent3,
+            Secondary = Colors.LightGreen.Darken3,
+            AppbarBackground = Colors.LightGreen.Accent3,
+            AppbarText = Colors.Shades.Black
+        },
+        PaletteLight = new PaletteLight()
+        {
+            Primary = "#1EFA2D",
+            Secondary = Colors.LightGreen.Darken1,
+            Background = Colors.Gray.Lighten1,
+            AppbarBackground = "#1EFA2D",
+            AppbarText = Colors.Shades.Black,
+            TextPrimary = Colors.Shades.Black,
+            PrimaryContrastText = Colors.Shades.Black,
+            DrawerText = Colors.Shades.Black,
+            DrawerBackground = Colors.LightGreen.Lighten4
+        }
+    };
+}
